7911ef283a0a2c96b469ef556aa35f26ff71f4b2
Carlos <Carlos@xtrene.com>
2021-06-21 19:47:57 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea04_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Los primeros 100 números pares: ");
+            while (num < 100)
+            {
+                num++;
+                if (num%2 == 0)
+                {
+                    Console.Write(" " + num);
+                }
+                
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
